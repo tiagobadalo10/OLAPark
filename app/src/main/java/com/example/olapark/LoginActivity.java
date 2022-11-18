a81f8c381e91f5369c1c8ccd702764073374bf99
tiagobadalo10 <tiagobadalo2014@gmail.com>
2022-11-18 16:29:43 +0000

    Start of main-menu activity

--- app/src/main/java/com/example/olapark/LoginActivity.java
+++ app/src/main/java/com/example/olapark/LoginActivity.java
@@ -13,6 +13,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // When login button is clicked
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(v -> {
+            Intent intent;
+            intent = new Intent(LoginActivity.this, MainMenuActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
         // When register button is clicked
         Button register = findViewById(R.id.register);
         register.setOnClickListener(v -> {
---
+++ app/src/main/java/com/example/olapark/MainMenuActivity.java
@@ -0,0 +1,52 @@
+package com.example.olapark;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.olapark.databinding.ActivityMainMenuBinding;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainMenuBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMainMenu.toolbar);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_menu);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_menu);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
