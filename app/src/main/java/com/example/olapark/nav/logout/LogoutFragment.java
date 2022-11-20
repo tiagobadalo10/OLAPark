5fa613d27f7572d48ae134043574248381cc17d8
tiagobadalo10 <tiagobadalo2014@gmail.com>
2022-11-20 00:19:02 +0000

    Start of Rewards

--- app/src/main/java/com/example/olapark/nav/logout/LogoutFragment.java
+++ app/src/main/java/com/example/olapark/nav/logout/LogoutFragment.java
@@ -1,12 +1,16 @@
 package com.example.olapark.nav.logout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
-
+import com.example.olapark.LoginActivity;
+import com.example.olapark.MainMenuActivity;
+import com.example.olapark.R;
 import com.example.olapark.databinding.FragmentLogoutBinding;
 
 public class LogoutFragment extends DialogFragment {
@@ -18,7 +22,29 @@ public class LogoutFragment extends DialogFragment {
 
         binding = FragmentLogoutBinding.inflate(inflater, container, false);
 
-        return binding.getRoot();
+        View v = binding.getRoot();
+
+        // Handling of logout buttons
+        onClickHandler(v);
+
+        return v;
     }
 
+    public void onClickHandler(View v){
+
+        Button yesButton = v.findViewById(R.id.yes);
+        Button noButton = v.findViewById(R.id.no);
+
+        // If user clicks on button yes
+        yesButton.setOnClickListener(view -> {
+            Intent i = new Intent(getContext(), LoginActivity.class);
+            startActivity(i);
+        });
+
+        // If user clicks on button no
+        noButton.setOnClickListener(view -> {
+            Intent i = new Intent(getContext(), MainMenuActivity.class);
+            startActivity(i);
+        });
+    }
 }
