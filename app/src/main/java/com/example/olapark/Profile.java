5a5a9b280c62e509179e91c9dc5a47c19d5b7254
tiagobadalo10 <tiagobadalo2014@gmail.com>
2022-11-23 23:34:14 +0000

    Profile activity

---
+++ app/src/main/java/com/example/olapark/Profile.java
@@ -0,0 +1,23 @@
+package com.example.olapark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.olapark.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent setIntent = new Intent(this, MainMenuActivity.class);
+        startActivity(setIntent);
+    }
+}
